b755e8a02571f146b35a4f2acccddb59e55fcf9f
your-github-username <manoj@mntfuture.com>
2025-06-16 11:32:19 +0530

    3rd changes6

--- src/contexts/BrandingContext.tsx
+++ src/contexts/BrandingContext.tsx
@@ -17,9 +17,7 @@ const BrandingContext = createContext<BrandingContextType | undefined>(undefined
 
 const getFileUrl = (path: string) => {
   if (!path) return '';
-  if (path.startsWith('http://') || path.startsWith('https://')) {
-    return path;
-  }
+  if (path.startsWith('http://') || path.startsWith('https://')) return path;
   return `${API_URL}${path}`;
 };
 
